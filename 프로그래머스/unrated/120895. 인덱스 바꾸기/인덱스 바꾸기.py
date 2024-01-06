db34b4fb622b1d3f56ea93811da010871c02dd51
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2024-01-06 12:51:37 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,5 +1,4 @@
 def solution(my_string, num1, num2):
-    answer = ''
-    fst, snd = my_string[num1], my_string[num2]
-    answer = my_string[:num1] + snd + my_string[num1+1:num2] + fst + my_string[num2 + 1:]
-    return answer
+    lst = list(my_string)
+    lst[num1], lst[num2] = lst[num2], lst[num1]
+    return ''.join(lst)
