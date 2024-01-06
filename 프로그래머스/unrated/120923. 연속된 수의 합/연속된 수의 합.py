480b7c8137b29c94da49bb85b7738bd8a8270b4a
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2024-01-06 17:40:12 +0900

    [level unrated] Title: 연속된 수의 합, Time: 0.08 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.py
@@ -0,0 +1,6 @@
+def solution(num, total):
+    start = total // num
+    for i in range(num):
+        result = range(start-i, start-i+num)
+        if sum(result) == total:
+            return list(result)
