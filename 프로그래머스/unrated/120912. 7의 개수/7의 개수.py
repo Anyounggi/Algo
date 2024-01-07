e3f85c9d014570830eac24a4ff2d8d1cd364a16d
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2024-01-07 09:21:39 +0900

    [level unrated] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.py
@@ -1,7 +1,2 @@
 def solution(array):
-    answer = 0
-    for num in array:
-        for st in str(num):
-            if st == "7":
-                answer += 1
-    return answer
+    return str(array).count('7')
