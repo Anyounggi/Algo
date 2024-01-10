1edf7b59bf38aae0fa140dc67032c3e15166ae03
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2024-01-10 21:12:00 +0900

    [level 3] Title: 야근 지수, Time: 393.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -0,0 +1,13 @@
+import heapq
+
+def solution(n, works):
+    works = [-work for work in works]
+    heapq.heapify(works)
+    
+    for _ in range(n):
+        work = heapq.heappop(works)
+        if not work:
+            heapq.heappush(works, work)
+            break
+        heapq.heappush(works, work + 1)
+    return sum([work**2 for work in works])
