3e49dc55b80a996b8430160bff18f5ad0de74f21
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2024-01-10 21:09:05 +0900

    [level 2] Title: 더 맵게, Time: 1920.28 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,13 @@
+import heapq
+
+def solution(scoville, K):
+    num_foods = len(scoville)
+    heapq.heapify(scoville)
+    score = heapq.heappop(scoville)
+    
+    idx = 0
+    while idx < num_foods - 1 and score < K:
+        heapq.heappush(scoville, score + heapq.heappop(scoville) * 2)
+        score = heapq.heappop(scoville)
+        idx += 1
+    return -1 if score < K else idx
