27774c22c37f4ce598351b7cea1e7a1f51db3858
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2024-02-25 08:25:30 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.71 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cs
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cs
@@ -4,7 +4,7 @@ public class Solution {
     public int solution(string num_str) {
         int answer = 0;
         foreach (char num in num_str) {
-            answer += (int)Char.GetNumericValue(num);
+            answer += Int32.Parse(num.ToString());
         }
         return answer;
     }
