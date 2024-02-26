aaf515a8091c2ce30988b665fd2c44e7d1823801
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2024-02-27 07:44:23 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.45 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cs
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public class Solution {
     public int[] solution(string my_string) {
-        if (my_string.Length < 1 || my_string.Length > 100)
-            throw new Exception("1 ≤ my_string의 길이 ≤ 100");
+        List<int> list = new List<int>();
+        foreach (var it in my_string)
+        {
+            if (int.TryParse(it.ToString(), out var value))
+            {
+                list.Add(value);
+            }
+        }
 
-        var numbers = my_string.Where(char.IsDigit).Select(x => int.Parse(x.ToString())).ToArray();
-        Array.Sort(numbers);
-        return numbers;
+        list.Sort();
+        return list.ToArray();
     }
 }
