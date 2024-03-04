c777381c57c8feb836190788c2a3899abde1d7ac
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2024-03-05 08:11:57 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.78 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cs
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cs
@@ -1,29 +1,10 @@
 using System;
 
 public class Solution {
-    public int solution(int[] date1, int[] date2) {
-        
-        int result=0;
-        if(date1 [0]<date2[0]) {
-            result=1;
-        } else if(date2[0]<date1[0]) {
-            result=0;
-        } else if(date2[0]==date1[0]) {
-            if(date1[1]<date2[1]) {
-                result=1;
-            } else if(date1[1]>date2[1]) {
-                result=0;
-            } else if(date1[1]==date2[1]) {
-                if(date1[2]<date2[2]) {
-                    result=1;
-                } else if(date1[2]>date2[2]) {
-                    result=0;
-                } else {
-                    result=0;
-                }
-            }
-        }
-        return result;
-        
+    public int solution(int[] date1, int[] date2) 
+    {
+        int d1 = int.Parse($"{date1[0]}{date1[1]}{date1[2]}");
+        int d2 = int.Parse($"{date2[0]}{date2[1]}{date2[2]}");
+        return d1 < d2 ? 1 : 0;
     }
 }
