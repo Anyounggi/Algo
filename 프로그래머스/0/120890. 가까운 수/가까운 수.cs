698e847010dd9bddc1fc717665e43d6ac6635552
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2024-03-28 08:02:05 +0900

    [level 0] Title: 가까운 수, Time: 0.44 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        int num = 100;
+        Array.Sort(array);
+        
+        for(int i = 0; i < array.Length; i++)
+        {
+            if(Math.Abs(array[i] - n) < num)
+            {
+                num = Math.Abs(array[i] - n);
+                answer = array[i];
+            }
+        }
+        return answer;
+    }
+}
